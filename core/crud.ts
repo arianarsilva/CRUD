201556cd2d94f99270428601c1a12c7a2ddac683
Ariana Silva <arianarebeca.silva@gmail.com>
2024-06-27 19:31:31 -0300

    add: function update()

--- core/crud.ts
+++ core/crud.ts
@@ -1,51 +1,82 @@
 import fs from "fs";
-// const fs = require("fs"); 
+import { v4 as uuid } from "uuid";
+
 const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
 interface Todo {
-    date: string;
-    content: string;
-    done: boolean;
+  id: string;
+  date: string;
+  content: string;
+  done: boolean;
 }
 
-function create(content: string) {
-    const todo: Todo = {
-        date: new Date().toISOString(),
-        content: content,
-        done: false,
-    };
+function create(content: string): Todo {
+  const todo: Todo = {
+    id: uuid(),
+    date: new Date().toISOString(),
+    content: content,
+    done: false,
+  };
 
-    const todos: Array<Todo> = [
-        ...read(),
-        todo,
-    ];
+  const todos: Array<Todo> = [...read(), todo];
 
-    // salvar o content no sistema
-    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+  // salvar o content no sistema
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
         todos,
-    }, null, 2
-    ));
-    return content;
+      },
+      null,
+      2
+    )
+  );
+  return todo;
 }
 
 function read(): Array<Todo> {
-    const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
-    const db = JSON.parse(dbString || "{}");
-    if (!db.todos) { // Fail Fast Validations
-        return [];
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}");
+  if (!db.todos) {
+    // Fail Fast Validations
+    return [];
+  }
+  return db.todos;
+}
+
+function update(id: string, partialTodo: Partial<Todo>) {
+  const todos = read();
+  todos.forEach((currentTodo) => {
+    const isToUpdate = currentTodo.id === id;
+    if (isToUpdate) {
+      Object.assign(currentTodo, partialTodo);
     }
-    return db.todos;
+  });
+
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos,
+      },
+      null,
+      2
+    )
+  );
 }
 
 function CLEAR_DB() {
-    fs.writeFileSync(DB_FILE_PATH, "")
+  fs.writeFileSync(DB_FILE_PATH, "");
 }
 
 // SIMULATION
 CLEAR_DB();
 create("Primeira TODO");
-create("Segunda TODO");
-create("Terceira TODO");
+create("Primeira TODO");
+const terceiraTodo = create("Segunda TODO");
+update(terceiraTodo.id, {
+  content: "Segunda TODO com novo content",
+});
 console.log(read());
