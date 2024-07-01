b96392852920855205fc5b04d22b93d5f7b7c5c2
Ariana Silva <arianarebeca.silva@gmail.com>
2024-07-01 17:37:50 -0300

    add: function deleteById()

--- core/crud.ts
+++ core/crud.ts
@@ -80,6 +80,27 @@ function updateContentById(id: string, content: string): Todo {
     })
 }
 
+function deleteById(id: string) {
+    const todos = read();
+    const todosWithoutOne = todos.filter((todo) => {
+        if(id === todo.id) {
+            return false
+        }
+        return true
+    })
+
+    fs.writeFileSync(
+        DB_FILE_PATH,
+        JSON.stringify(
+          {
+            todos: todosWithoutOne,
+          },
+          null,
+          2
+        )
+      );
+}
+
 function CLEAR_DB() {
   fs.writeFileSync(DB_FILE_PATH, "");
 }
@@ -87,11 +108,14 @@ function CLEAR_DB() {
 // SIMULATION
 CLEAR_DB();
 create("Primeira TODO");
-create("Primeira TODO");
-const terceiraTodo = create("Segunda TODO");
+const secondTodo = create("Segunda TODO");
+deleteById(secondTodo.id);
+const thirdTodo = create("Terceira TODO");
 // update(terceiraTodo.id, {
 //   content: "Atualizada!",
 //   done: true,
 // });
-updateContentById(terceiraTodo.id, "Atualizada!") 
+updateContentById(thirdTodo.id, "Atualizada!")
+const todos = read();
 console.log(read());
+console.log(todos.length);
